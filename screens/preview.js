8e5b4fe0df3810cd3a1fe963a1e656e4253cceba
Oana Tomuta <tomutaoana@gmail.com>
2021-05-10 22:35:33 +0300

    added preview slider for app info

---
+++ screens/preview.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import {View, Text, StyleSheet, StatusBar, Image, TouchableOpacity} from 'react-native';
+import AppIntroSlider from "react-native-app-intro-slider";
+
+const slides = [
+    {
+        key: "one",
+        title: "KEEP IT ORGANIZED",
+        text: "Buy any available cryptocurrency with real cash right in the app or sell it.",
+        image: require('../images/icon_organized.png')
+    },
+    {
+        key: "two",
+        title: "WALLET MANAGEMENT",
+        text: "Store and manage all your digital currencies with ease without any worries.",
+        image: require('../images/icon_wallet.png')
+    },
+    {
+        key: "three",
+        title: "SAFE PROCESS",
+        text: "Change your cryptocurrencies to other currencies or fiat, safely and realiably.",
+        image: require('../images/icon_safe.png')
+    }
+]
+
+export default class Preview extends React.Component {
+    constructor() {
+        super();
+    }
+
+    componentDidMount() {
+
+    }
+
+    _renderItem = ({item}) =>{
+        return(
+            <View style={{ flex:1, alignItems:'center', justifyContent:'center'}}>
+                <Image source={item.image} resizeMode="cover" style={{width:150,height:150, marginVertical:'5%'}}/>
+                <Text style={{color:'white', fontSize:24, fontFamily:'bold-font'}}>{item.title}</Text>
+                <Text style={{color:'white', fontSize:18, fontFamily:'normal-font', marginHorizontal:'15%', textAlign:'center'}}>{item.text}</Text>
+            </View>
+        )
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <StatusBar style="auto" />
+                <View style={{ flex:0.08, justifyContent:'center', alignItems:'flex-end',marginRight:'5%'}}>
+                    <TouchableOpacity onPress={ () => this.props.navigation.navigate('SignUp') }>
+                        <Text style={{color:'white', fontSize:18, fontFamily:'bold-font'}}>
+                            SKIP
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+                <AppIntroSlider
+                    renderItem={this._renderItem}
+                    data = {slides}
+                    activeDotStyle={{backgroundColor:"#00a0d4"}}
+                    dotStyle={{backgroundColor:'#55597c'}}
+                    nextLabel=""
+                    doneLabel=""
+                />
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor:'#303463'
+    },
+});
