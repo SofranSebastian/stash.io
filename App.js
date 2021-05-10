f57dcefab0d136fac606cab708deb90ef47938c8
Oana Tomuta <tomutaoana@gmail.com>
2021-05-10 22:36:22 +0300

    implemented AppLoading library

--- App.js
+++ App.js
@@ -1,31 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
+import AppLoading from 'expo-app-loading';
+import * as Font from 'expo-font';
+
+import MyStack from './routes/routes.js';
+
+const customFonts = {
+  'light-font' : require('./fonts/Helvetica-Light.ttf'),
+  'normal-font' : require('./fonts/Helvetica-Normal.ttf'),
+  'bold-font' : require('./fonts/Helvetica-Bold.ttf')
+};
 export default class App extends React.Component {
   constructor() {
     super();
+
+    this.state = {
+      areFontsLoaded : false
+    }
   }
 
-  componentDidMount() {
-    
+  async _loadFontsAsync(){
+    await Font.loadAsync(customFonts)
+    this.setState({areFontsLoaded: true})
+  }
+
+  async componentDidMount() {
+    this._loadFontsAsync();
   }
 
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </View>
-    );
+    if(this.state.areFontsLoaded){
+      return(
+          <MyStack/>
+      )
+    }else{
+      return <AppLoading/>
+    }
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
