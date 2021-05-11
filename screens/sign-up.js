afd86f439094a6338ec599fa37e4be1c2d0a2fb0
SofranSebastian <sofransebastian@yahoo.com>
2021-05-11 11:10:50 +0300

    sign-up.js: created the handled for the sign up with firebase(with dummy data)

--- screens/sign-up.js
+++ screens/sign-up.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import {View, Text, StyleSheet, StatusBar, Image} from 'react-native';
+import {View, Text, StyleSheet, StatusBar, Alert, TouchableOpacity} from 'react-native';
 
+import * as firebase from "firebase";
 export default class SignUp extends React.Component {
     constructor() {
         super();
@@ -10,11 +11,21 @@ export default class SignUp extends React.Component {
 
     }
 
+    _handleSignUp = () => {
+      firebase.auth()
+              .createUserWithEmailAndPassword("sofransebastian@yahoo.com", '123456')
+              .then( () => console.log("Users Registered succesfully!"))
+              .catch( error => console.log(error.message))
+    }
+
     render() {
         return (
             <View style={styles.container}>
                 <StatusBar style="auto" />
                 <Text>SignUP</Text>
+                <TouchableOpacity onPress={this._handleSignUp}>
+                  <Text>SIGNUP</Text>
+                </TouchableOpacity>
             </View>
         );
     }
