ece3bf1a3ecc895f7f90f7baff20d5c34009fda8
SofranSebastian <sofransebastian@yahoo.com>
2021-05-11 11:11:48 +0300

    sign-up.js: added the Alert component when an error occurs

--- screens/sign-up.js
+++ screens/sign-up.js
@@ -15,7 +15,22 @@ export default class SignUp extends React.Component {
       firebase.auth()
               .createUserWithEmailAndPassword("sofransebastian@yahoo.com", '123456')
               .then( () => console.log("Users Registered succesfully!"))
-              .catch( error => console.log(error.message))
+              .catch( error => Alert.alert("Error",
+                                            error.message,
+                                            [
+                                              {
+                                                text:'Ok',
+                                                onPress: () => console.log("Ok Pressed")
+                                              },
+                                              {
+                                                text:'Try Again',
+                                                onPress: () => console.log("Try Again Pressed"),
+                                                style: 'cancel'
+                                              }
+                                            ]
+
+                              )
+                    )
     }
 
     render() {
