3f9ac83ce8100604ad3eff6f04225146d8ec09b3
SofranSebastian <sofransebastian@yahoo.com>
2021-05-14 23:33:52 +0300

    transfer.js: added the functionality for: -getting the amount for each part of the transfer and update it -handling all use cases of the inputs -added a modal for user confirmation after seeing all the data set by him

--- screens/transfer.js
+++ screens/transfer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text, StyleSheet, StatusBar, Alert, TouchableOpacity, Image, TextInput, Dimensions, ScrollView} from 'react-native';
+import {View, Text, StyleSheet, StatusBar, Alert, TouchableOpacity, Image, Modal, TextInput, Dimensions, ScrollView} from 'react-native';
 import DropDownPicker from 'react-native-dropdown-picker';
 
 import AsyncStorage from '@react-native-async-storage/async-storage'; 
@@ -7,6 +7,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import * as firebase from "firebase";
 
 export default class Transfer extends React.Component {
+    
     constructor() {
         super();
 
@@ -18,7 +19,9 @@ export default class Transfer extends React.Component {
             usernameEntered: '',
             emailFromUser: '',
             currenciesData: [],
-            usersArray: []
+            usersArray: [],
+            oldCurrenciesData : {},
+            isModalVisible: false,
         }
     }
 
@@ -101,15 +104,116 @@ export default class Transfer extends React.Component {
         this._handleGetAllUsers()
     }
 
+
+
     _handlerTransferCurrency = () =>{
+
         if(( this.state.usernameEntered !== '' ) &&
            ( this.state.amountEntered !== '') &&
            ( this.state.chosenCurrencyValue !== 0)
-        ){
-            console.log('E ok')
+        ){              
+            if( this.state.usersArray.includes(this.state.usernameEntered) === false){
+                Alert.alert("Error",
+                                "The user does not exist.",
+                                [
+                                    {
+                                        text:'Try Again',
+                                        onPress: () => console.log("Try Again Pressed"),
+                                        style: 'cancel'
+                                    }
+                                ]
+                            )
+            }else{
+
+                if( this.state.amountEntered > this.state.chosenCurrencyValue){
+                    Alert.alert("Error",
+                                "The amount is bigger than what you have.",
+                                [
+                                    {
+                                        text:'Try Again',
+                                        onPress: () => console.log("Try Again Pressed"),
+                                        style: 'cancel'
+                                    }
+                                ]
+                            )
+                }else{
+                   
+
+                            this.setState({isModalVisible:!this.state.isModalVisible})
+                            
+                            firebase.database()
+                            .ref('/users')
+                            .once('value')
+                            .then((snapshot) =>{
+                    
+                                var temporary_object = {};
+                    
+                                snapshot.forEach( (childSnapshot) => {
+                    
+                                    if( childSnapshot.val().username === this.state.usernameEntered ){
+                                        
+                                        temporary_object = childSnapshot.val().currencies
+                                    }
+                                })
+                                this.setState({oldCurrenciesData:temporary_object})
+                            })
+                         
+                            
+                }
+            }
         }else{
-            console.log('nu a completat tot')
+            Alert.alert("Error",
+                                "Please complete all the fields before sending.",
+                                [
+                                    {
+                                        text:'Try Again',
+                                        onPress: () => console.log("Try Again Pressed"),
+                                        style: 'cancel'
+                                    }
+                                ]
+                            )
         }
+    }
+
+
+    _handlerConfirmTransfer = () => {
+
+                        let path = '/users/' + this.state.usernameEntered + '/currencies'
+                        let path_me = '/users/' + this.state.emailFromUser + '/currencies'
+
+                        let value = 0
+                        let value_me = 0
+
+                        if(this.state.chosenCurrency === 'USD'){
+
+                            value = Number(this.state.oldCurrenciesData.USD) +  Number(this.state.amountEntered)
+                            value_me = this.state.chosenCurrencyValue - this.state.amountEntered
+                         
+
+                            firebase.database()
+                                    .ref(path)
+                                    .update({
+                                        USD: value
+                                    })
+
+                            firebase.database()
+                                    .ref(path_me)
+                                    .update({
+                                        USD: value_me
+                                    })
+                        }
+                            
+
+                            Alert.alert("Success!",
+                                "The transaction is done.",
+                                [
+                                    {
+                                        text:'Ok',
+                                        onPress: () => console.log("Ok Pressed"),
+                                    }
+                                ]
+                            )
+                            this.props.navigation.reset({index:0, routes:[{name:"Transfer"}]});
     }
 
     render() {
@@ -174,7 +278,7 @@ export default class Transfer extends React.Component {
                                                 alignItems:'center', 
                                                 justifyContent:'center'
                                         }}
-                                        onPress={()=>this._handlerTransferCurrency()}  
+                                        onPress={this._handlerTransferCurrency}  
                     >
                         <Text style={{color:'white', fontSize:14, fontFamily:'bold-font'}}>SEND</Text>
                     </TouchableOpacity>
@@ -182,6 +286,47 @@ export default class Transfer extends React.Component {
                         *Be careful who you transfer to because the process is not reversible.*
                     </Text>
                 </View>
+                <Modal  animationType="slide"
+                            visible={this.state.isModalVisible}
+                            transparent
+                    >
+                    <View style={{  flex: 1,
+                                    flexDirection: 'column',
+                                    justifyContent: 'center',
+                                    alignItems: 'center',
+                                    backgroundColor:'#000000000000',
+                    }}>
+                        <View style={{  backgroundColor: "#00a0d4",
+                                        flex:0.95,
+                                        width: '90%',
+                                        borderRadius:10,
+                                        alignItems:'center',
+                                        justifyContent:'center',
+                            }}>
+                            <View style={{flex:0.2, justifyContent:'center', alignItems:'center',marginTop:'5%'}}>
+                                    <Image source={require('../images/icon_logo.png')} style={{width:50, height:50}}/>
+                                    <Text style={{color:'white', fontSize:24, fontFamily:'bold-font', marginTop:'5%'}}>BUY & HODL</Text>
+                            </View>
+                            <View style={{flex:0.6, width:'100%', alignItems:'center'}}>
+                                <Text>TO : {this.state.usernameEntered}</Text>
+                                <Text>AMOUNT : {this.state.amountEntered}</Text>
+                                <Text>AMOUNT : {this.state.oldCurrenciesData.USD}</Text>
+                            </View>
+                            <View style={{flexDirection:'row', width:'75%', justifyContent:'space-around',flex:0.2, alignItems:'center'}}>
+                                <TouchableOpacity   style={{height:40, width:80, backgroundColor:"#1a6594", borderRadius:5, alignItems:'center', justifyContent:'center'}}
+                                                    onPress={this._handlerConfirmTransfer}
+                                >
+                                    <Text style={{color:'white', fontSize:14, fontFamily:'bold-font'}}>CONFIRM</Text>
+                                </TouchableOpacity>
+                                <TouchableOpacity   style={{height:40, width:80, backgroundColor:'#0f1434', borderRadius:5, alignItems:'center', justifyContent:'center'}}
+                                                    onPress={()=>this.setState({isModalVisible:false})}
+                                >
+                                    <Text style={{color:'white', fontSize:14, fontFamily:'bold-font'}}>DISMISS</Text>
+                                </TouchableOpacity>
+                            </View>
+                        </View>
+                    </View>
+                </Modal>
             </View>
         );
     }
